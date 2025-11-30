98647fd7e51840b30fd64617ace5983139740713
Jordan Carr <jccarr3@ncsu.edu>
2025-11-29 19:44:58 -0500

    gradescope submission 9

--- sim_proc.cc
+++ sim_proc.cc
@@ -111,7 +111,7 @@ int main (int argc, char* argv[])
 
     //fetch section
     while(advance_cycle()){
-        if(current_cycle > 15000) break;
+        //if(current_cycle > 15000) break;
         test_count++;
         
         //if(test_count % 50 == 0) break;
